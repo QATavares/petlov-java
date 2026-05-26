8c61e196fd82ab4674f4300ac8a8a248605a7601
Rodrigo Tavares <rtsantana@outlook.com.br>
2026-05-26 14:47:11 -0300

    Modelagem da massa de testes

--- src/test/java/br/com/rocketskills/petlov/Cadastro.java
+++ src/test/java/br/com/rocketskills/petlov/Cadastro.java
@@ -9,21 +9,49 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
+class PontoDoacao {
+	String nome;
+	String email;
+	String cep;
+	Integer numero;
+	String complemento;
+	String pets;
+
+	public PontoDoacao(String nome, String email, String cep, Integer numero, String complemento, String pets) {
+		this.nome = nome;
+		this.email = email;
+		this.cep = cep;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.pets = pets;
+	}
+	
+}
 class Cadastro {
 
 	@Test
 	@DisplayName("Deve cadastrar um ponto de doação")
 	void createPoint() {
+
+		PontoDoacao ponto = new PontoDoacao(
+			"Estação Pet",
+			"estacao@pet.com.br",
+			"71655520",
+			888,
+			"Complemento teste",
+			"Cachorros"
+		);
+
 		open("https://petlov.vercel.app/signup");
 		$("h1").shouldHave(text("Cadastro de ponto de doação"));
 
-		$("input[placeholder='Nome do ponto de doação']").setValue("Estação Pet");
-		$("input[name=email]").setValue("estacao@pet.com.br");
-		$("input[name=cep]").setValue("71655520");
+		$("input[placeholder='Nome do ponto de doação']").setValue(ponto.nome);
+		$("input[name=email]").setValue(ponto.email);
+		$("input[name=cep]").setValue(ponto.cep);
 		$("input[type=button]").click();
-		$("input[name=addressNumber").setValue("888");
-		$("input[name=addressDetails]").setValue("Complemento teste");
-		$(By.xpath("//span[text()=\"Cachorros\"]/..")).click();
+		$("input[name=addressNumber").setValue(ponto.numero.toString());
+		$("input[name=addressDetails]").setValue(ponto.complemento);
+		$(By.xpath("//span[text()=\"" + ponto.pets + "\"]/..")).click();
 		$(".button-register").click();
 	
 		String target = "Seu ponto de doação foi adicionado com sucesso. Juntos, podemos criar um mundo onde todos os animais recebam o amor e cuidado que merecem.";
